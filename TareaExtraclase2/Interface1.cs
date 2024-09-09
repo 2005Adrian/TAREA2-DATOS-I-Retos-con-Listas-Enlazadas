d2aa35440051f53b7dabbe54584350f9c083b460
DESKTOP-T8SP4NE\Adrian Pereira <a.pereira.2@estudiantec.cr>
2024-09-09 17:40:45 -0600

    Agregar archivos de proyecto.

---
+++ TareaExtraclase2/Interface1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaExtraclase2
+{
+    public interface IList
+    {
+        void InsertInOrder(int value);
+        int DeleteFirst();
+        int DeleteLast();
+        bool DeleteValue();
+        int GetMidddle();
+
+        void MergeSorted(IList listA, IList listB, ListSortDirection direction);
+    }
+
+
+    public enum SortDirection
+
+    {
+        Ascending, Descending
+    }
+}
