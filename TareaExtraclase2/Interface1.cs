5344c966f17559285c725efbe3daa1c65cf79120
DESKTOP-T8SP4NE\Adrian Pereira <a.pereira.2@estudiantec.cr>
2024-09-09 19:15:02 -0600

    pruebas no funcionan

--- TareaExtraclase2/Interface1.cs
+++ TareaExtraclase2/Interface1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +14,10 @@ namespace TareaExtraclase2
         void InsertInOrder(int value);
         int DeleteFirst();
         int DeleteLast();
-        bool DeleteValue();
-        int GetMidddle();
+        bool DeleteValue(int value);
+        int GetMiddle();
 
-        void MergeSorted(IList listA, IList listB, ListSortDirection direction);
+        void MergeSorted(IList listA, IList listB, SortDirection direction);
     }
 
 
@@ -24,4 +26,240 @@ namespace TareaExtraclase2
     {
         Ascending, Descending
     }
+
+    public class ListaDoble : IList
+    {
+        public class Nodo
+        {
+            public int Valor;
+            public Nodo Siguiente;
+            public Nodo Anterior;
+
+            public Nodo(int valor)
+            {
+                Valor = valor;
+                Siguiente = null;
+                Anterior = null;
+
+
+
+            }
+
+
+        }
+        public Nodo cabeza;
+        public Nodo cola;
+
+        public ListaDoble()
+        {
+            cabeza = null;
+            cola = null;
+        }
+        
+
+
+        public int DeleteFirst()
+        {
+            if (cabeza == null)
+                throw new InvalidOperationException("LA LISTA ESTA VACIA");
+            int valor = cabeza.Valor;
+            cabeza = cabeza.Siguiente;
+
+            if (cabeza != null)
+            {
+                cabeza.Anterior = null;
+
+            }
+            else
+                cola = null;
+            return valor; 
+        }
+
+        public int DeleteLast()
+        {
+            if (cola == null)
+
+                throw new InvalidCastException("lista vacia");
+
+            int valor = cola.Valor;
+            cola = cola.Anterior;
+
+            if (cola != null)
+                cola.Siguiente = null;
+            else
+                cabeza = null;
+            return valor;   
+
+        }
+        public bool DeleteValue (int value)
+        {
+            Nodo actual = cabeza;
+
+            while (actual != null && actual.Valor != value)
+            {
+                actual = actual.Siguiente;
+            }
+
+            if (actual == null)
+                return false;
+
+            if (actual.Anterior != null)
+                actual.Anterior.Siguiente = actual.Siguiente;
+            else
+                cabeza = actual.Siguiente;
+
+            if (actual.Siguiente != null)
+                actual.Siguiente.Anterior = actual.Anterior;
+            else
+                cola = actual.Anterior;
+            return true;
+        }
+
+
+        //Problema 1 
+
+        public void MergeSorted(IList listA, IList listB, SortDirection direction)
+        {
+            Nodo currentA = ((ListaDoble)listA).cabeza;
+            Nodo currentB = ((ListaDoble)listB).cabeza;
+            Nodo nuevoNodo = null;
+
+            // Limpiar la lista actual
+            cabeza = null;
+            cola = null;
+
+            while (currentA != null && currentB != null)
+            {
+                if ((direction == SortDirection.Ascending && currentA.Valor <= currentB.Valor) ||
+                    (direction == SortDirection.Descending && currentA.Valor >= currentB.Valor))
+                {
+                    nuevoNodo = new Nodo(currentA.Valor);
+                    currentA = currentA.Siguiente;
+                }
+                else
+                {
+                    nuevoNodo = new Nodo(currentB.Valor);
+                    currentB = currentB.Siguiente;
+                }
+
+                InsertarNodo(nuevoNodo);
+            }
+
+            // Agregar los elementos restantes de listA, si hay
+            while (currentA != null)
+            {
+                nuevoNodo = new Nodo(currentA.Valor);
+                InsertarNodo(nuevoNodo);
+                currentA = currentA.Siguiente;
+            }
+
+            // Agregar los elementos restantes de listB, si hay
+            while (currentB != null)
+            {
+                nuevoNodo = new Nodo(currentB.Valor);
+                InsertarNodo(nuevoNodo);
+                currentB = currentB.Siguiente;
+            }
+        }
+
+
+        private void InsertarNodo(Nodo nuevoNodo)
+        {
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+                cola = nuevoNodo;
+            }
+            else
+            {
+                cola.Siguiente = nuevoNodo;
+                nuevoNodo.Anterior = cola;
+                cola = nuevoNodo;
+            }
+        }
+
+        //PROBLEMA 2 
+        public void Invert()
+        {
+            Nodo actual = cabeza;
+            Nodo temp = null;
+
+            while (actual != null)
+            {
+                // Intercambiar los punteros Siguiente y Anterior
+                temp = actual.Anterior;
+                actual.Anterior = actual.Siguiente;
+                actual.Siguiente = temp;
+
+                actual = actual.Anterior; // Moverse hacia el siguiente nodo, que ahora es el anterior
+            }
+
+            // Al final del ciclo, temp es la nueva cabeza
+            if (temp != null)
+            {
+                cabeza = temp.Anterior; // temp.Anterior apunta al nuevo primer nodo
+            }
+        }
+
+        //PROBLEMA 3
+        public void InsertInOrder(int value)
+        {
+            Nodo nuevoNodo = new Nodo(value);
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+                cola = nuevoNodo;
+                return;
+            }
+            if (value < cabeza.Valor)
+            {
+                nuevoNodo.Siguiente = cabeza;
+                cabeza.Anterior = nuevoNodo;
+                cabeza = nuevoNodo;
+                return;
+            }
+
+            Nodo actual = cabeza;
+            while (actual.Siguiente != null && actual.Siguiente.Valor < value)
+            {
+                actual = actual.Siguiente;
+            }
+
+            nuevoNodo.Siguiente = actual.Siguiente;
+            if (actual.Siguiente != null)
+            {
+                actual.Siguiente.Anterior = nuevoNodo;
+
+            }
+            else
+            {
+                cola = nuevoNodo;
+
+            }
+            actual.Siguiente = nuevoNodo;
+            nuevoNodo.Anterior = actual;
+
+        }
+        public int GetMiddle()
+        {
+            if (cabeza == null)
+                throw new InvalidOperationException("lista vacia");
+            Nodo lento = cabeza;
+            Nodo rapido = cabeza;
+
+            while (rapido != null && rapido.Siguiente != null)
+            {
+                rapido = rapido.Siguiente.Siguiente;
+                lento = lento.Siguiente;
+            }
+            return lento.Valor;
+
+        }
+
+
+
+    }
+
 }
+
--- TestTareaDatosEXTRACLASE2/UnitTest1.cs
+++ TestTareaDatosEXTRACLASE2/UnitTest1.cs
@@ -1,11 +1,132 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TareaExtraclase2;  // Asegúrate de que este es el espacio de nombres correcto
+
 namespace TestTareaDatosEXTRACLASE2
 {
     [TestClass]
     public class UnitTest1
     {
+        // Problema 1: InsertInOrder, DeleteFirst, DeleteLast, DeleteValue, GetMiddle, MergeSorted
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestInsertInOrder()
         {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            Assert.AreEqual(5, lista.cabeza.Valor);
+            Assert.AreEqual(10, lista.cabeza.Siguiente.Valor);
+            Assert.AreEqual(15, lista.cola.Valor);
+        }
+
+        [TestMethod]
+        public void TestDeleteFirst()
+        {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            int eliminado = lista.DeleteFirst();
+            Assert.AreEqual(5, eliminado);
+            Assert.AreEqual(10, lista.cabeza.Valor);
+        }
+
+        [TestMethod]
+        public void TestDeleteLast()
+        {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            int eliminado = lista.DeleteLast();
+            Assert.AreEqual(15, eliminado);
+            Assert.AreEqual(10, lista.cola.Valor);
+        }
+
+        [TestMethod]
+        public void TestDeleteValue()
+        {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            bool eliminado = lista.DeleteValue(10);
+            Assert.IsTrue(eliminado);
+            Assert.AreEqual(5, lista.cabeza.Valor);
+            Assert.AreEqual(15, lista.cola.Valor);
+        }
+
+        [TestMethod]
+        public void TestGetMiddle()
+        {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            int middle = lista.GetMiddle();
+            Assert.AreEqual(10, middle);
+        }
+
+        [TestMethod]
+        public void TestMergeSortedAscending()
+        {
+            ListaDoble listaA = new ListaDoble();
+            listaA.InsertInOrder(10);
+            listaA.InsertInOrder(5);
+            listaA.InsertInOrder(15);
+
+            ListaDoble listaB = new ListaDoble();
+            listaB.InsertInOrder(20);
+            listaB.InsertInOrder(3);
+            listaB.InsertInOrder(7);
+
+            ListaDoble listaMerge = new ListaDoble();
+            listaMerge.MergeSorted(listaA, listaB, SortDirection.Ascending);
+
+            Assert.AreEqual(3, listaMerge.cabeza.Valor);
+            Assert.AreEqual(20, listaMerge.cola.Valor);
+        }
+
+        [TestMethod]
+        public void TestMergeSortedDescending()
+        {
+            ListaDoble listaA = new ListaDoble();
+            listaA.InsertInOrder(10);
+            listaA.InsertInOrder(5);
+            listaA.InsertInOrder(15);
+
+            ListaDoble listaB = new ListaDoble();
+            listaB.InsertInOrder(20);
+            listaB.InsertInOrder(3);
+            listaB.InsertInOrder(7);
+
+            ListaDoble listaMerge = new ListaDoble();
+            listaMerge.MergeSorted(listaA, listaB, SortDirection.Descending);
+
+            Assert.AreEqual(20, listaMerge.cabeza.Valor);
+            Assert.AreEqual(3, listaMerge.cola.Valor);
+        }
+
+        // Problema 2: Invertir la lista
+        [TestMethod]
+        public void TestInvertList()
+        {
+            ListaDoble lista = new ListaDoble();
+            lista.InsertInOrder(10);
+            lista.InsertInOrder(5);
+            lista.InsertInOrder(15);
+
+            lista.Invert();
+
+            Assert.AreEqual(15, lista.cabeza.Valor);
+            Assert.AreEqual(5, lista.cabeza.Siguiente.Valor);
+            Assert.AreEqual(10, lista.cola.Valor);
         }
     }
 }
